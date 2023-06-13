162c196ec2654c94646f1fe78bc8f9c7a666f7ed
harryduong99 <duongminhnam99@gmail.com>
2023-06-13 18:57:29 +0700

    add: stack task

---
+++ stack/bestDigits.m.js
@@ -0,0 +1,13 @@
+function bestDigits(number, numDigits) {
+  // the order after removal can not be changed => can use stack
+  let stack = {};
+  for (let i = 0; i < number.length; i++) {
+    
+  }
+
+  for (let j = 0; j < numDigits; j++) {
+    
+  }
+
+  return "";
+}
